ce08638b2b29f99fb4fdb92beb31893323c77dec
Felipe Vogel <fps.vogel@gmail.com>
2024-10-19 15:43:32 -0400

    Fix total count and percent in `arb progress`

--- lib/arb/cli/progress.rb
+++ lib/arb/cli/progress.rb
@@ -3,7 +3,7 @@ module Arb
     def self.progress
       WorkingDirectory.prepare!
 
-      past_year_count = 25 * (Date.today.year - 1 - 2015)
+      past_year_count = 25 * (Date.today.year - 2015)
       this_year_count = (Date.today.month == 12 ? Date.today.day : 0).clamp(..25)
       total_count = past_year_count + this_year_count
 
@@ -11,7 +11,7 @@ module Arb
       my_total_count = my_counts_by_year.values.sum
 
       total_percent = (my_total_count.to_f / total_count * 100).round(1)
-      total_percent == total_percent.to_i ? total_percent.to_i : total_percent
+      total_percent = total_percent == total_percent.to_i ? total_percent.to_i : total_percent
 
       puts "You have completed:\n\n"
       puts PASTEL.bold "#{PASTEL.blue("All:")}\t#{total_percent}% \t#{my_total_count}/#{total_count} puzzles\n"
